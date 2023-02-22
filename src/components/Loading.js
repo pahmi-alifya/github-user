f364a15dc0b148b4a4ee7bed7b036ee18d1c58c8
Pahmi Alifya Bahri <pahmi.bahri@mncgroup.com>
2023-02-22 19:55:05 +0700

    create: components

---
+++ src/components/Loading.js
@@ -0,0 +1,6 @@
+import React, { memo } from "react";
+
+const Loading = () => {
+  return <div className="font-bold text-3xl">Loading...</div>;
+};
+export default memo(Loading);
---
+++ src/components/Profile.js
@@ -0,0 +1,78 @@
+import React, { memo } from "react";
+import { format } from "date-fns";
+
+const Profile = (props) => {
+  return (
+    <>
+      <article className="bg-white p-5 rounded shadow shadow-white">
+        <div className="flex items-center">
+          <img
+            src={props.owner.avatar_url}
+            alt={props.owner.login}
+            className="w-16 h-16 shadow rounded-full"
+          />
+          <ul className="ml-5">
+            <li>
+              <h2 className="font-bold text-xl">{props.owner.login}</h2>
+            </li>
+            <div>
+              <p className="mr-2">{props.name}</p>
+              {props.private ? (
+                <p className="bg-rose-700 py-1 px-2 rounded-lg shadow text-white text-xs inline-block opacity-75">
+                  Private
+                </p>
+              ) : (
+                <p className="bg-emerald-700 py-1 px-2 rounded-lg shadow text-white text-xs inline-block opacity-75 mr-2">
+                  Public
+                </p>
+              )}
+            </div>
+          </ul>
+        </div>
+
+        <div>
+          <p className="mt-5">
+            This repository was created on{" "}
+            {format(new Date(props.created_at), "dd MMMM yyyy")} by{" "}
+            {props.owner.login}
+          </p>
+        </div>
+
+        <div className="mt-5 flex items-center justify-between text-right">
+          <a
+            className="underline text-sm"
+            href={props.html_url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            View Repo
+          </a>
+          <ul>
+            <li>{props.stargazers_count.toLocaleString()} stars</li>
+            <li>{props.watchers_count.toLocaleString()} Watchers</li>
+          </ul>
+        </div>
+
+        <div className="flex items-center justify-between flex-wrap mt-5 overflow-scroll">
+          <ul className="text-xs flex items-center justify-start">
+            <li className="py-1 px-2 text-white bg-blue-500 opacity-75 rounded-lg shadow inline-block mr-2">
+              {props.language ?? "-"}
+            </li>
+
+            {props?.topics?.map((topic, index) => (
+              <div key={index}>
+                <li className="py-1 px-2 text-white bg-red-500 opacity-75 rounded-lg shadow inline-block mr-2">
+                  {topic}
+                </li>
+              </div>
+            ))}
+          </ul>
+
+          <p>{props.open_issues} issues</p>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default memo(Profile);
